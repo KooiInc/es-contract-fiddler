c12888cfa266444ac9ef6f48cd319ea3bb0494ea
Renzo Kooi <renzo.kooi@gmail.com>
2024-04-12 10:40:32 +0200

    typeofany module update

--- typofany.module.js
+++ typofany.module.js
@@ -1,48 +1,84 @@
-export default IS;
+export {IS as default, maybe};
 
-function IS(obj, ...shouldBe) {
-  const undefs = [`NaN`, `null`, `undefined`];
-  const maybeSymbol = something => typeof something === `symbol` ? Symbol : something;
+function IS(anything, ...shouldBe) {
+  const input =  typeof anything === `symbol` ? Symbol('any') : anything;
+  return shouldBe.length > 1 ? ISOneOf(input, ...shouldBe) : determineType(input, ...shouldBe);
+}
 
-  return shouldBe.length > 1 ? ISOneOf(obj, ...shouldBe) : determineType();
-
-  function determineType() {
-    const objOrSymbol = maybeSymbol(obj);
-    const shouldBeLen = shouldBe.length > 0;
-    shouldBe = shouldBeLen && shouldBe.shift();
-    const shouldBeIsNothing = shouldBeLen && isNothing(shouldBe);
-
+function determineType(input, ...shouldBe) {
+  let { compareWith, inputIsNothing, shouldBeIsNothing, inputCTOR, is_NAN } = getVariables(input, ...shouldBe);
+  
+  if (is_NAN && compareWith) {
+    compareWith = maybe({trial:  _ => String(compareWith), whenError: _ => ``});
+    return compareWith === String(input) || shouldBe === Number;
+  }
+  
+  if (inputIsNothing || shouldBeIsNothing) {
     return shouldBeIsNothing
-      ? `${objOrSymbol}` === `${shouldBe}` :
-      obj === false
-        ? shouldBe === Boolean ? true : `Boolean` :
-        thisIs()
-
-    function thisIs() {
-      const self = obj === 0
-        ? Number : obj === ``
-          ? String : !objOrSymbol
-            ? {name: `${objOrSymbol}`} : Object.getPrototypeOf(obj)?.constructor;
-
-      return shouldBe
-        ? shouldBe === self ||
-          shouldBe === Object.getPrototypeOf(self) ||
-          `${shouldBe}` === self?.name
-        : self?.name;
-    }
+      ? String(input) === String(compareWith)
+      : !compareWith
+        ? `${input}`
+        : false;
   }
+  
+  if (inputCTOR === Boolean) {
+    return !compareWith ? `Boolean` : !!(inputCTOR === compareWith);
+  }
+  
+  return getResult(input, compareWith, getMe(input, inputCTOR));
+}
 
-  function isNothing(nothing) {
-    for (let nada of undefs) {
-      if (nada === `${nothing}`) { return true; }
-    }
+function getMe(input, inputCTOR) {
+  return input === 0
+    ? Number : input === ``
+      ? String : !input
+        ? {name: String(input)} : inputCTOR;
+}
+
+function getVariables(input, ...shouldBe) {
+  const sbLen = shouldBe.length > 0;
+  const compareWith = sbLen && shouldBe.shift();
+  const inputIsNothing = isNothing(input);
+  const shouldBeIsNothing = sbLen && isNothing(compareWith);
+  const inputCTOR = !inputIsNothing && Object.getPrototypeOf(input)?.constructor;
+  const is_NAN = maybe({trial: _ => String(input), whenError: _ => ``}) === `NaN`;
+  
+  return { compareWith, inputIsNothing, shouldBeIsNothing, inputCTOR, is_NAN };
+}
+
+function getResult(input, shouldBe, self) {
+  if (maybe({trial:  _ => String(shouldBe), whenError: _ => `-`}) === `NaN`) {
+    return String(input) === `NaN`;
+  }
+  
+  return shouldBe
+    ? maybe({
+      trial: _ => !!(input instanceof shouldBe),
+      whenError: _ => false } ) ||
+    shouldBe === self ||
+    shouldBe === Object.getPrototypeOf(self) ||
+    `${shouldBe?.name}` === self?.name
+    : self?.name;
+}
+
+function ISOneOf(obj, ...params) {
+  for (const param of params) {
+    if (IS(obj, param))  { return true; }
+  }
+  return false;
+}
+
+function isNothing(maybeNothing) {
+  return maybe({
+    trial: _ => /^(undefined|null)$/.test(String(maybeNothing)),
+    whenError: _ => false });
+}
+
+function maybe({trial, whenError = err => console.log(err) } = {}) {
+  if (!trial || !(trial instanceof Function)) {
+    console.info(`TypeofAnything {maybe}: trial parameter not a Function or Lambda`);
     return false;
   }
-
-  function ISOneOf(obj, ...params) {
-    for (let param of params) {
-      if (IS(obj, param))  { return true; }
-    }
-    return false;
-  }
+  
+  try { return trial(); } catch(err) { return whenError(err); }
 }
